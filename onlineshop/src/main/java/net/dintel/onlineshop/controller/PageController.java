da0c0517ec3e5e1fb78af88242925af18a916c1a
Pawel <pawelzyl@wp.pl>
2018-06-02 18:20:46 +0200

    Poprawne zaladowanie statycznych danych

--- onlineshop/src/main/java/net/dintel/onlineshop/controller/PageController.java
+++ onlineshop/src/main/java/net/dintel/onlineshop/controller/PageController.java
@@ -1,6 +1,7 @@
 package net.dintel.onlineshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,4 +16,26 @@ public class PageController {
 		return mv;
 	}
 
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting){
+//		if(greeting == null){
+//			greeting = "Hello there";
+//		}
+//		
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable(value="greeting", required=false)String greeting){
+		if(greeting == null){
+			greeting = "Hello there";
+		}
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+	
 }
